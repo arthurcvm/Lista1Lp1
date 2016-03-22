38d0c669f81dd1ccb2bee74806edd4f3ddd3bcff
Arthur Cezar <arthurcvm@hotmail.com>
2016-03-22 19:08:29 -0300

    Q12

--- src/lista1lp1/Q12.java
+++ src/lista1lp1/Q12.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lista1lp1;
 
-/**
- *
- * @author Arthur Cezar
- */
+import java.util.Scanner;
+
 public class Q12 {
-    
+    static int tam = 10;
+    public static void main (String[] args){
+        char sexo[] = new char [tam];
+        double altura[] = new double [tam];
+        
+        Scanner le = new Scanner(System.in);
+        
+        
+    }
 }
